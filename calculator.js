85918b491b91db3e1d0abf9a4ff3fa0e04c97001
JusRecondo <justina.recondo@gmail.com>
2021-04-22 13:48:07 -0300

    mejoras en codigo ejemplo

--- calculator.js
+++ calculator.js
@@ -1,17 +1,21 @@
-document.getElementById('calculator').addEventListener('change', function() {
-  let bill = Number(document.getElementById('billTotal').value);
-  let tipPercent = document.getElementById('tipInput').value;
-  let split = document.getElementById('splitInput').value;
+const calculator = document.querySelector('#calculator');
+const billTotalInput = document.querySelector('#billTotal');
+const tipInput = document.querySelector('#tipInput');
+const splitInput = document.querySelector('#splitInput');
+
+calculator.addEventListener('input', function() {
+  let bill = parseFloat(billTotalInput.value);
+  let tipPercent = tipInput.value;
+  let split = splitInput.value;
   let billEach = bill / split;
   let tipValue = bill * (tipPercent/100);
   let newBillEach = (bill + tipValue) / split;
   let tipAmountEach = tipValue / split;
 
-  document.getElementById('billEach').innerHTML = billEach.toFixed(2);
-  document.getElementById('tipOutput').innerHTML = tipPercent + "%";
-  document.getElementById('splitOutput').innerHTML = split;
-  document.getElementById('newBill').innerHTML = "$" + newBillEach.toFixed(2);
-  document.getElementById('tipAmount').innerHTML = "$" + tipAmountEach.toFixed(2);
-
+  document.querySelector('#billEach').innerHTML = billEach.toFixed(2);
+  document.querySelector('#tipOutput').innerHTML = tipPercent + "%";
+  document.querySelector('#splitOutput').innerHTML = split;
+  document.querySelector('#newBill').innerHTML = " $" + newBillEach.toFixed(2);
+  document.querySelector('#tipAmount').innerHTML = " $" + tipAmountEach.toFixed(2);
 
 } );
